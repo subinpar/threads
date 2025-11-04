804e9658461dfba54f99852c58d796a7259a8c17
subinpar <bagsubin565@gmail.com>
2025-11-04 20:12:01 +0900

    하단 탭 적용

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,57 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs, useRouter } from "expo-router";
+import {Ionicons} from "@expo/vector-icons";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+export default function TabLayout(){
+    const router=useRouter();
+    return <Tabs
+    screenOptions={{headerShown: false,}}>
+        <Tabs.Screen 
+            name="index"
+            options={{
+                tabBarLabel:()=>null,
+                tabBarIcon: ({focused})=>(
+                 <Ionicons name="home" size={24} color={focused ? "black":"gray"}/>
+        ),
         }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+        />
+        <Tabs.Screen 
+            name="search"
+            options={{
+                tabBarLabel:()=>null,
+                tabBarIcon: ({focused})=>(
+                 <Ionicons name="search" size={24} color={focused ? "black":"gray"}/>
+        ),
+        }}/>
+        <Tabs.Screen
+            name="add"
+            listeners={{
+                tabPress:(e)=>{
+                    e.preventDefault();
+                    router.navigate("/modal");
+;                }
+            }
+            }
+            options={{
+                tabBarLabel:()=>null,
+                tabBarIcon: ({focused})=>(
+                 <Ionicons name="add" size={24} color={focused ? "black":"gray"}/>
+        ),
+        }}/>
+        <Tabs.Screen 
+        name="activity"
+          options={{
+                tabBarLabel:()=>null,
+                tabBarIcon: ({focused})=>(
+                 <Ionicons name="heart-outline" size={24} color={focused ? "black":"gray"}/>
+        ),
+        }}/>
+        <Tabs.Screen 
+        name="[username]"
+          options={{
+                tabBarLabel:()=>null,
+                tabBarIcon: ({focused})=>(
+                 <Ionicons name="person-outline" size={24} color={focused ? "black":"gray"}/>
+        ),
+        }}/>
+     </Tabs>
 }
