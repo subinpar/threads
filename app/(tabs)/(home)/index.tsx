f27c75d1e3326addc5c7520ce8596f1683b14b40
subinpar <bagsubin565@gmail.com>
2025-11-06 18:00:55 +0900

    css 적용

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,46 +1,105 @@
-import { Text, View } from "react-native";
-import { TouchableOpacity } from "react-native";
+import { BlurView } from "expo-blur";
 import { usePathname, useRouter } from "expo-router";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
-  const router=useRouter();
-  const pathname=usePathname();
-
-  console.log(pathname);
+  const router = useRouter();
+  const pathname = usePathname();
+  const insets = useSafeAreaInsets();
+  const isLoggedIn = false;
 
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={[
+        styles.container,
+        { paddingTop: insets.top, paddingBottom: insets.bottom },
+      ]}
     >
-        <View>
-          <TouchableOpacity onPress={() => router.push(`/`)}>
-            <Text style={{color: pathname === '/' ? 'red' : 'black'}}>For you</Text>
+      <BlurView style={styles.header} intensity={70}>
+        <Image
+          source={require("../../../assets/images/react-logo.png")}
+          style={styles.headerLogo}
+        />
+        {!isLoggedIn && (
+          <TouchableOpacity
+            style={styles.loginButton}
+            onPress={() => {
+              console.log("loginButton onPress");
+              router.navigate(`/login`);
+            }}
+          >
+            <Text style={styles.loginButtonText}>로그인</Text>
           </TouchableOpacity>
+        )}
+      </BlurView>
+      {isLoggedIn && (
+        <View style={styles.tabContainer}>
+          <View style={styles.tab}>
+            <TouchableOpacity onPress={() => router.navigate(`/`)}>
+              <Text style={{ color: pathname === "/" ? "red" : "black" }}>
+                For you
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.tab}>
+            <TouchableOpacity onPress={() => router.navigate(`/following`)}>
+              <Text style={{ color: pathname === "/" ? "black" : "red" }}>
+                Following
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View>
-          <TouchableOpacity onPress={() => router.push(`/following`)}>
-            <Text style={{color: pathname === '/' ? 'black' : 'red'}}>Following</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <TouchableOpacity onPress={() => router.push(`/@subin/post/1`)}>
-            <Text>게시글1</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <TouchableOpacity onPress={() => router.push(`/@subin/post/2`)}>
-            <Text>게시글2</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <TouchableOpacity onPress={() => router.push(`/@subin/post/3`)}>
-            <Text>게시글3</Text>
-          </TouchableOpacity>
-        </View>
+      )}
+      <View>
+        <TouchableOpacity onPress={() => router.push(`/@zerocho/post/1`)}>
+          <Text>게시글1</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity onPress={() => router.push(`/@zerocho/post/2`)}>
+          <Text>게시글2</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity onPress={() => router.push(`/@zerocho/post/3`)}>
+          <Text>게시글3</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  tabContainer: {
+    flexDirection: "row",
+  },
+  tab: {
+    flex: 1,
+    alignItems: "center",
+  },
+  header: {
+    alignItems: "center",
+  },
+  headerLogo: {
+    width: 42, // DP, DIP
+    height: 42,
+  },
+  loginButton: {
+    position: "absolute",
+    right: 20,
+    top: 0,
+    backgroundColor: "black",
+    borderWidth: 1,
+    borderColor: "black",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+  },
+  loginButtonText: {
+    color: "white",
+  },
+});
